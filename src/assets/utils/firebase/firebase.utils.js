4ffdedb82334ea63a5c02050bb2bc4b3e8aab502
ViKaite <viktorija.zelionkaite@gmail.com>
2023-05-30 20:44:08 +0300

    added inputs

--- src/assets/utils/firebase/firebase.utils.js
+++ src/assets/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
     getAuth, 
     signInWithRedirect, 
     signInWithPopup, 
-    GoogleAuthProvider
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword
 } from 'firebase/auth';
 import{
    getFirestore,
@@ -25,17 +26,22 @@ const firebaseConfig = {
   
   const firebaseApp = initializeApp(firebaseConfig);
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
-    prompt: "select_account"
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
+    prompt: 'select_account',
   });
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => 
+  signInWithRedirect(auth, googleProvider);
   
   export const db = getFirestore();
 
   export const createUserDocumentFromAuth = async (userAuth) => {
+    if(!userAuth) return;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     console.log(userDocRef)
@@ -60,5 +66,10 @@ const firebaseConfig = {
     }
 
      return userDocRef;
+  };
 
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password)
   }
